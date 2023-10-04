32a414dd8ec39a41de6200affd6a0b6aa16e4ee1
tk4125 <87638000+tk4125@users.noreply.github.com>
2023-10-04 15:16:13 +0900

    bash file update

--- RaspberryPiPrograms/PythonPrograms.sh
+++ RaspberryPiPrograms/PythonPrograms.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo chmod 777 /dev/gpiomem
+
 python3 /home/mamorukun/hukuyakumamoru-main/RaspberryPiPrograms/reset-istakemedicines.py &
 python3 /home/mamorukun/hukuyakumamoru-main/RaspberryPiPrograms/PutMedicine.py &
 python3 /home/mamorukun/hukuyakumamoru-main/RaspberryPiPrograms/TakeOutMedicine.py
