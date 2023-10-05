2ffecf1abd2224c4a703ccadde292c466e01879f
tk4125 <87638000+tk4125@users.noreply.github.com>
2023-10-05 14:41:11 +0900

    gpio pin program update

--- RaspberryPiPrograms/TakeOutMedicine.py
+++ RaspberryPiPrograms/TakeOutMedicine.py
@@ -63,7 +63,7 @@ d_today =  str(datetime.date.today())
 
 PIN = 17
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN, initial=GPIO.LOW)
 
 while 1:
     while 1:
