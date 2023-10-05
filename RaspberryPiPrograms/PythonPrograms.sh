3fd5e1984c3e27e8a3ba166d30bb0c09a2a6142a
tk4125 <87638000+tk4125@users.noreply.github.com>
2023-10-05 17:04:37 +0900

    Delete RaspberryPiPrograms directory

--- RaspberryPiPrograms/PythonPrograms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo chmod 777 /dev/gpiomem
-
-python3 /home/mamorukun/hukuyakumamoru-main/RaspberryPiPrograms/reset-istakemedicines.py &
-python3 /home/mamorukun/hukuyakumamoru-main/RaspberryPiPrograms/PutMedicine.py &
-python3 /home/mamorukun/hukuyakumamoru-main/RaspberryPiPrograms/TakeOutMedicine.py
