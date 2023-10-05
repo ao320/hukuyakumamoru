5229332bd4835b9f8be79ab7942c8d98a2677f0b
tk4125 <87638000+tk4125@users.noreply.github.com>
2023-10-05 17:23:21 +0900

    cupdavice update

--- RaspberryPiPrograms/ReciveCupdate.py
+++ RaspberryPiPrograms/ReciveCupdate.py
@@ -2,8 +2,76 @@ import datetime
 import locale
 from pymongo import MongoClient
 import urllib.request, json
+import asyncio
+from bleak import BleakScanner, BleakClient
 
-#
+# ESP32のデバイスを識別するためのUUID 
+#ESP32_UUIDs = ["34:85:18:9D:72:59"]
+ESP32_UUIDs = ["C0:49:EF:67:FD:DA"]
+
+# Nordic UART Service (NUS)
+#NUS_UUID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e'
+#RX_UUID = '93222d1f-2837-4f1d-88d0-e30b6d1935e1'
+global gyro
+global date
+gyro = ""
+date = [0, 0]
+
+
+# コールバック関数: データが送信されたときに呼び出される
+def notification_handler(sender: int, data: bytearray, **_kwargs):
+    global gyro
+    global date
+    gyro = data.decode()
+    
+    if gyro[0] == "1":
+        date[0] = int(gyro[2:])
+    else:
+        date[1] = int(gyro[2:])
+
+async def run():
+
+    # 1. 周囲のBLE発信をスキャン
+    scanner = BleakScanner()
+    devices = await scanner.discover()
+
+    clients = []
+    for device in devices:
+        print(f'name:{device.name},address:{device.address}')
+        if device.address in ESP32_UUIDs:
+            client = BleakClient(device)
+            clients.append(client)
+
+
+    try:
+        
+        # 2. クライアント（ESP32などのデバイス）とデータのやり取りをする
+        for client in clients:
+            await client.connect()
+
+            # Characteristicの情報を得る
+            for service in client.services:
+                print('---------------------')
+                print(f"service uuid:{service.uuid}, description:{service.description}")
+                [print(f'{c.properties},{c.uuid}',) for c in service.characteristics]
+            
+            #await client.start_notify('93222d1f-2837-4f1d-88d0-e30b6d1935e1', notification_handler)
+                await client.start_notify('cd4e5793-eb97-4364-8193-4f5fcb7ef69b', notification_handler)
+            
+        global date
+        while True:
+            if date[0] >= 45 or date[1] >= 45:
+                break
+            # 実際のアプリケーションではここで何らかの処理
+            await asyncio.sleep(1.0)
+    finally:
+        print(14)
+        # for client in clients:
+        #     await client.stop_notify(RX_UUID)
+        #     await client.disconnect()
+
+asyncio.run(run())
+
 
 # db接続
 client = MongoClient('mongodb://160.16.222.38:22238')
